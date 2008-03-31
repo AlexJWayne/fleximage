019dd1fe5e48b4e14b19cf0711995ae1d3b63de1
Alex Wayne <alex@beautifulpixel.com>
2008-03-30 22:09:04 -0700

    documentation

--- lib/fleximage/model.rb
+++ lib/fleximage/model.rb
@@ -27,11 +27,33 @@ module Fleximage
       
       # Returns the path to the master image file for this record.
       #   
-      #   @some_image
+      #   @some_image.file_path #=> /var/www/myapp/uploaded_images
       def file_path
         "#{RAILS_ROOT}/#{self.class.image_directory}/#{id}.png"
       end
       
+      # Sets the image file for this record to an uploaded file.  This can 
+      # be called directly, or passively like from an ActiveRecord mass 
+      # assignment.
+      # 
+      # Rails will automatically call this method for you, in most of the 
+      # situations you would expect it to.
+      #
+      #   # Direct Assignment, usually not needed
+      #   photo = Photo.new
+      #   photo.image_file = params[:photo][:image_file]
+      #   
+      #   # via explicit assignment hash
+      #   Photo.new(:image_file => params[:photo][:image_file])
+      #   Photo.create(:image_file => params[:photo][:image_file])
+      #   
+      #   # via mass assignment from, the most common form you'll probably use
+      #   Photo.new(params[:photo])
+      #   Photo.create(params[:photo])
+      #   
+      #   # via an association proxy
+      #   p = Product.find(1)
+      #   p.images.create(params[:photo])
       def image_file=(file)
         if file.respond_to?(:read) && file.size > 0
           
@@ -49,11 +71,15 @@ module Fleximage
         end
       end
       
-      def load_image
+      # Load the image from disk, or return the cached and potentially 
+      # processed output rmagick image.
+      def load_image #:nodoc:
         @output_image ||= Magick::Image.read(file_path).first
       end
       
-      def output_image
+      # Convert the current output image to a jpg, and return it in 
+      # binary form.
+      def output_image #:nodoc:
         @output_image.format = 'JPG'
         @output_image.to_blob
       end
