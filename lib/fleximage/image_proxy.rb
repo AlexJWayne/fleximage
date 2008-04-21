371b930be0ae30dd1b4c911bb1ece4a3cd0ece57
Alex Wayne <alex@beautifulpixel.com>
2008-04-21 11:09:52 -0700

    Added Fleximage::Operator::Base tests, as well as some slight cleanup to Fleximage::ImageProxy

--- lib/fleximage/image_proxy.rb
+++ lib/fleximage/image_proxy.rb
@@ -17,25 +17,25 @@ module Fleximage
     # The image to be manipulated by operators.
     attr_accessor :image
     
-    # Create a new image operator proxy.  Just provide the name of the image
+    # Create a new image operator proxy.
     def initialize(image, model_obj)
       @image = image
       @model = model_obj
     end
     
     # A call to an unknown method will look for an Operator by that method's name.
-    # If it finds one, it will execute that operator, otherwise it will simply call super for the
-    # default method missing behavior.
+    # If it finds one, it will execute that operator.
     def method_missing(method_name, *args)
       # Find the operator class
-      operator_class = "Fleximage::Operator::#{method_name.to_s.camelcase}".constantize
+      class_name = method_name.to_s.camelcase
+      operator_class = "Fleximage::Operator::#{class_name}".constantize
       
       # Execute the operator
       @image = operator_class.new(self, @image, @model).execute(*args)
     
     rescue NameError => e
-      if e.to_s =~ /uninitialized constant Fleximage::Operator::#{method_name.to_s.camelcase}/
-        raise OepratorNotFound, "No correspoding operator found for the method \"#{method_name}\""
+      if e.to_s =~ /uninitialized constant Fleximage::Operator::#{class_name}/
+        raise OepratorNotFound, "No operator Fleximage::Operator::#{class_name} found for the method \"#{method_name}\""
       else
         raise e
       end
