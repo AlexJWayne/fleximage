95f9b4f85fc890e9defd1dd5c57e193dec1450e4
Joshua Abbott <joshnabbott@mac.com>
2009-01-28 13:51:20 +0800

    added gravity support to crop method
    Signed-off-by: Andrew White <andyw@pixeltrix.co.uk>

--- lib/fleximage/operator/crop.rb
+++ lib/fleximage/operator/crop.rb
@@ -8,10 +8,12 @@ module Fleximage
     #
     # Use the following keys in the +options+ hash:
     #
+    # * +gravity+: Select gravity for the crop. Default is :top_left (Magick::NorthWestGravity)
+    #   Choose from GRAVITITES constant defined in base.rb.
     # * +from+: coorinates for the upper left corner of resulting image.
     # * +size+: The size of the resulting image, going down and to the right of the :+from+ coordinate.
     # 
-    # Both options are *required*.
+    #  size and from options are *required*.
     #
     # Example:
     #
@@ -21,9 +23,18 @@ module Fleximage
     #       :size => '500x350'
     #     )
     #   end
+    # - or -
+    #   @photo.operate do |image|
+    #     image.crop(
+    #       :gravity => :center
+    #       :from    => '100x50',
+    #       :size    => '500x350'
+    #     )
+    #   end
     class Crop < Operator::Base
       def operate(options = {})
         options = options.symbolize_keys
+        options.reverse_merge!(:gravity => :top_left)
 
         # required integer keys
         [:from, :size].each do |key|
@@ -37,7 +48,7 @@ module Fleximage
         end
 
         # crop
-        @image.crop!(options[:from][0], options[:from][1], options[:size][0], options[:size][1], true)
+        @image.crop!(GRAVITIES[options[:gravity]], options[:from][0], options[:from][1], options[:size][0], options[:size][1], true)
       end
     end
     
